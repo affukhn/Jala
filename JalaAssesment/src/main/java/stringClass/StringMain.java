6b4001a86f0a5bc0049a6870bef3765990c70cbe
DESKTOP-SCAFAFP\user <arfkhnsuk@gmail.com>
2024-08-28 14:47:20 +0530

    work More

--- JalaAssesment/src/main/java/stringClass/StringMain.java
+++ JalaAssesment/src/main/java/stringClass/StringMain.java
@@ -1,6 +1,7 @@
 package stringClass;
 
 import java.nio.Buffer;
+import java.util.Locale;
 import java.util.regex.Pattern;
 
 public class StringMain {
@@ -20,9 +21,23 @@ public class StringMain {
          System.out.println(s.length()+" length of the string");
          System.out.println(s.substring(0,4));
          System.out.println(s.indexOf('d'));
+         String email = "myEMail@exa.com";
+         String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+         boolean isValid = email.matches(regex);
+         System.out.println("Is the email valid? " + isValid);
          System.out.println("Hello".equals("Hello"));
          System.out.println("Hello".equals("World"));
          System.out.println("Hello".equalsIgnoreCase("hello"));
+         System.out.println("Hello".startsWith("He"));
+         System.out.println("World".endsWith("ld"));
+         System.out.println("Hello".compareTo("hello"));
+         String TimStr=" WOrk done ";
+         System.out.println(TimStr.trim());
+         System.out.println(TimStr.replace('O','o'));
+         System.out.println(String.valueOf("232"));
+         System.out.println(10+"");
+         System.out.println(s.toUpperCase());
+         System.out.println(s.toLowerCase());
 
 
 
